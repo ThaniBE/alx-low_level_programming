f0f359d5bd8be6692e23f85cc2b849c1bc2e7d2a
ThaniBE <barodaemmanuelthani@gmail.com>
2022-09-13 00:41:27 -0700

    Single digit numbers in ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 48;
+	while (num <= 57)
+	{
+	       	putchar(num);
+		num++;
+	}
+	putchar(10);
+	return (0);
+}
