88e431b023b21e044117720bda51075a52bb620e
ThaniBE <barodaemmanuelthani@gmail.com>
2022-09-12 22:41:00 -0700

    Print lowercase alphabet letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alpbt;
+	alpbt = 'a';
+	while ((alpbt == 'a') || (alpbt <= 'z'))
+	{	
+		putchar(alpbt);
+		alpbt++;
+
+	}
+	putchar('\n');
+	return (0);
+}
