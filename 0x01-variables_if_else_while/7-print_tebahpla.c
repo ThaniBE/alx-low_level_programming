7c71dc3bdb5a403171a77f0a3301ff910410b7a4
ThaniBE <barodaemmanuelthani@gmail.com>
2022-09-14 12:54:11 -0700

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int rvs_ltt;
+
+	rvs_ltt = 'z';
+	while(rvs_ltt == 'z' || rvs_ltt >= 'a')
+	{
+		putchar(rvs_ltt);
+		rvs_ltt--;
+	}
+	putchar('\n');
+	return (0);
+}
