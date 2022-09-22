5e5617fbbc9b7e035274a097fd6db67e25c36651
ThaniBE <barodaemmanuelthani@gmail.com>
2022-09-22 03:08:54 -0700

    print multiple times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i, a;
+
+	i = 48;
+	while (i < 58)
+	{
+		
+		a = 97;
+		while ( a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		i++;
+		_putchar(10);
+	}
+}
