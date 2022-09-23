7bad67bc5d0ddfb79fc20775a778a02ca0763d5c
ThaniBE <barodaemmanuelthani@gmail.com>
2022-09-22 20:57:44 -0700

    lower or upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: lower or uper case letter
+ * Return: 1 if lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c < 97 || c > 122)
+	       if (c < 65 || c > 90)
+	{
+	for (c = 97, c = 65; c < 122 || c < 90; c++)
+		return (0);
+	}
+	return (1);
+}
